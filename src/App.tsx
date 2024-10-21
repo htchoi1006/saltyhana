38a9ddedaa3909ef06561e69a1aafb803d6938c8
commaengddongyeon <kimdongyn927@naver.com>
2024-10-21 14:31:26 +0900

    add.route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import styled from "styled-components";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SingupPage/SingupPage";
+import HomePage from "./pages/HomePage/HomePage";
+import CalendarPage from "./pages/CalendarPage/CalendarPage";
+import GoalPage from "./pages/GoalPage/GoalPage";
+import AssetsPage from "./pages/AssetsPage/AssetsPage";
+import RecommendPage from "./pages/RecommendPage/RecommendPage";
+import TestPage from "./pages/TestPage/TestPage";
+import Consumption from "./pages/ConsumptionPage/ConsumptionPage";
+import Result from "./pages/ResultPage/ResultPage";
 
 const AppContainer = styled.div`
   font-family: Arial, sans-serif;
@@ -21,9 +29,15 @@ const App: React.FC = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/goal" element={<GoalPage />} />
+            <Route path="/assets" element={<AssetsPage />} />
+            <Route path="/recommend" element={<RecommendPage />} />
+            <Route path="/test" element={<TestPage />} />
+            <Route path="/test/consumption" element={<Consumption />} />
+            <Route path="/result/consumption" element={<Result />} />
           </Routes>
-
-          <div>abd</div>
         </AppContainer>
       </Router>
     </>
