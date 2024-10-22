429e7b93c98630c5ef946d3acb26ec06aef2d76a
gxxhxx0224 <gxxhxx0224@gmail.com>
2024-10-22 11:38:09 +0900

    pre-commit 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,30 @@ import styled from "styled-components";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SingupPage/SingupPage";
+import TeststartPage from "./pages/TeststartPage/TeststartPage";
 
 const AppContainer = styled.div`
-	font-family: Arial, sans-serif;
-	max-width: 800px;
-	margin: 0 auto;
-	padding: 20px;
+  font-family: Arial, sans-serif;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 `;
 
 const App: React.FC = () => {
-	return (
-		<>
-			<Router>
-				<AppContainer>
-					<Routes>
-						<Route path="/" element={<LandingPage />} />
-						<Route path="/login" element={<LoginPage />} />
-						<Route path="/signup" element={<SignupPage />} />
-					</Routes>
-				</AppContainer>
-			</Router>
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <AppContainer>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/teststart" element={<TeststartPage />} />
+          </Routes>
+        </AppContainer>
+      </Router>
+    </>
+  );
 };
 
 export default App;
