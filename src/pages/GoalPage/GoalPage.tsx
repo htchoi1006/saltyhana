fd214f94442bf39a7dbe09a816fc17580c895555
htchoi1006 <htchoi1006@kookmin.ac.kr>
2024-10-22 13:17:34 +0900

    feat. 사이드 바 기능 개발

--- src/pages/GoalPage/GoalPage.tsx
+++ src/pages/GoalPage/GoalPage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const GoalPage: React.FC = () => {
-  return <div>목표설정 페이지입니다.</div>;
+  return (
+    <>
+      <div>목표설정 페이지입니다.</div>
+    </>
+  );
 };
 
 export default GoalPage;
