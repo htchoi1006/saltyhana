00d3032aaec0c00d4417aa859151671cef7f4f65
magae1 <lws6665@naver.com>
2024-10-22 15:24:25 +0900

    feat. 회원가입 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import SignupPage from "./pages/SingupPage/SingupPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 import HomePage from "./pages/HomePage/HomePage";
 import CalendarPage from "./pages/CalendarPage/CalendarPage";
 import GoalPage from "./pages/GoalPage/GoalPage";
