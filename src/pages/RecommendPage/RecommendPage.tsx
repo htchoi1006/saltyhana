7abf341a40f94c382cc4d17ed818587e6af0bccc
commaengddongyeon <kimdongyn927@naver.com>
2024-10-23 13:41:14 +0900

    feat. 상품 추천 페이지

--- src/pages/RecommendPage/RecommendPage.tsx
+++ src/pages/RecommendPage/RecommendPage.tsx
@@ -1,11 +1,134 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import * as styled from "./styles";
+import travelLogImage from "../../images/recommend_travellog.png";
+import sweatHanaImage from "../../images/recommend_sweathana.png";
+import youthAccountImage from "../../images/recommend_youthaccount.png";
+import counselIcon from "../../images/recommend_counsel.png";
+import { ReactComponent as WalletIcon } from "../../images/recommend_wallet.svg";
+import { ReactComponent as RecommendIcon } from "../../images/recommend_producticon.svg";
 
+// RecommendPage 컴포넌트: 추천 페이지를 렌더링하는 함수형 컴포넌트
 const RecommendPage: React.FC = () => {
   return (
     <div>
-      상품추천 페이지입니다.
-      <Link to="/test/consumption">테스트 하러 가기</Link>
+      {/* 전체 페이지의 레이아웃 */}
+      <styled.BodyWrapper>
+        {/* 상품 리스트가 표시되는 섹션 */}
+        <styled.ProductList>
+          {/* 트래블로그 여행 적금 카드 */}
+          <styled.ProductWrapper>
+            <styled.ProductCard>
+              {/* 상품 이미지 */}
+              <styled.ProductCardImage
+                src={travelLogImage}
+                alt="트레블로그 여행 적금"
+              />
+              {/* 카드의 타이틀 */}
+              <styled.ProductCardTitle>
+                트레블로그 여행 적금
+              </styled.ProductCardTitle>
+              {/* 카드의 부제목 */}
+              <styled.ProductCardSubTitle>
+                여행 준비의 시작
+              </styled.ProductCardSubTitle>
+              {/* 상품 정보 */}
+              <styled.ProductInfo>
+                연(세전, 5년)
+                <br />
+                4.50% ~ 6.00%
+              </styled.ProductInfo>
+            </styled.ProductCard>
+            {/* 카드 아래에 표시될 타이틀 */}
+            <styled.ProductTitle>트래블로그 여행 적금</styled.ProductTitle>
+          </styled.ProductWrapper>
+
+          {/* 달달 하나 적금 카드 */}
+          <styled.ProductWrapper>
+            <styled.ProductCard>
+              <styled.ProductCardImage src={sweatHanaImage} alt="달달하나" />
+              <styled.ProductCardTitle>달달 하나</styled.ProductCardTitle>
+              <styled.ProductInfo>
+                혜택(2백만원까지)
+                <br />
+                기본 0.10%~최고 3.00%(연, 세전)
+              </styled.ProductInfo>
+            </styled.ProductCard>
+            <styled.ProductTitle>달달하나 적금</styled.ProductTitle>
+          </styled.ProductWrapper>
+
+          {/* 이미지 크기 수정 요망 */}
+          {/* 
+          <styled.ProductWrapper>
+            <styled.ProductCard >
+              <styled.ProductCardTitle>달달 하나</styled.ProductCardTitle>
+              <styled.ProductInfo>
+                혜택(2백만원까지)
+                <br />
+                기본 0.10%~최고 3.00%(연, 세전)
+              </styled.ProductInfo>
+            </styled.ProductCard>
+            <styled.ProductTitle>달달하나 적금</styled.ProductTitle>
+          </styled.ProductWrapper> */}
+
+          {/* 하나 청년도약 계좌 */}
+          <styled.ProductWrapper>
+            <styled.ProductCard>
+              <styled.ProductCardImage
+                src={youthAccountImage}
+                alt="청년도약계좌"
+              />
+              <styled.ProductCardTitle>
+                하나 청년도약 계좌
+              </styled.ProductCardTitle>
+              <styled.ProductCardSubTitle>
+                하나와 함께 도약
+              </styled.ProductCardSubTitle>
+              <styled.ProductInfo>
+                연(세전, 5년)
+                <br />
+                4.50% ~ 6.00%
+              </styled.ProductInfo>
+            </styled.ProductCard>
+            <styled.ProductTitle>하나 적금</styled.ProductTitle>
+          </styled.ProductWrapper>
+
+          {/* 추천 적금 */}
+          <styled.RecommendProduct>
+            <styled.RecommendProductText>
+              홍길동님께
+              <br />
+              추천하는 적금
+            </styled.RecommendProductText>
+            <RecommendIcon />
+          </styled.RecommendProduct>
+
+          {/* 소비성향 테스트 */}
+          <styled.TestCard>
+            <styled.TestCardText>
+              <Link
+                to="/test/consumption"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                소비성향
+                <br />
+                Test
+              </Link>
+            </styled.TestCardText>
+            <WalletIcon />
+          </styled.TestCard>
+
+          {/* 고객센터 버튼 */}
+          <styled.CounselBox>
+            <styled.IconWrapper>
+              {/* 고객센터 아이콘 */}
+              <styled.IconImage src={counselIcon} alt="고객센터 아이콘" />
+            </styled.IconWrapper>
+            {/* 고객센터 텍스트 */}
+            <styled.CounselText>고객센터</styled.CounselText>
+          </styled.CounselBox>
+        </styled.ProductList>
+      </styled.BodyWrapper>
     </div>
   );
 };
