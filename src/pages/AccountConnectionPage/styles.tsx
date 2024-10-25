71340d9aa150a8ca1c1ab816b645665262f9b610
haneul0509 <haneul0509@naver.com>
2024-10-25 11:33:03 +0900

    feat. 계좌연결완료 페이지 개발

---
+++ src/pages/AccountConnectionPage/styles.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Img = styled.img`
+  position: relative;
+  margin-top: 11%;
+  left: 45%;
+  top: 65%;
+  width: 200px;
+  height: 200px;
+`;
+
+export const MainText = styled.p`
+  position: relative;
+  width: 423px;
+  height: 20px;
+  left: 40%;
+  top: 70%;
+
+  font-family: "Noto Sans KR";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 33px;
+  line-height: 150%;
+
+  color: #000000;
+`;
