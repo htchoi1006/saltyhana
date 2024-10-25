cf264efe193b87644c02f2488c7891a15b032566
6jieun <mjkim12160@gmail.com>
2024-10-25 10:31:52 +0900

    add. nav 연결, 모달 컴포넌트 분리

--- src/components/Modals/ModalsBackground.tsx
+++ src/components/Modals/ModalsBackground.tsx
@@ -1,4 +1,4 @@
-import { useState, ReactNode, Children } from "react";
+import { ReactNode } from "react";
 import ModalPortal from "../../helpers/Portal";
 
 import { BgModal, ModalContainer } from "./styles";
@@ -9,31 +9,19 @@ interface ModalsProps {
 }
 
 const ModalsBackground = ({ onClose, children }: ModalsProps) => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  // 모달 닫기 함수
-  const handleClose = () => {
-    setIsOpen(false);
-    onClose();
-  };
-
   // 모달 바깥을 클릭하면 모달 닫기
   const handleBgClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
-      handleClose();
+      onClose();
     }
   };
 
   return (
-    <>
-      {isOpen && (
-        <ModalPortal>
-          <BgModal onClick={handleBgClick}>
-            <ModalContainer>{children}</ModalContainer>
-          </BgModal>
-        </ModalPortal>
-      )}
-    </>
+    <ModalPortal>
+      <BgModal onClick={handleBgClick}>
+        <ModalContainer>{children}</ModalContainer>
+      </BgModal>
+    </ModalPortal>
   );
 };
 
