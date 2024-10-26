122fd1fad2cf30892aeebfdd11997f9158d63db0
magae1 <lws6665@naver.com>
2024-10-26 13:48:34 +0900

    fix. 랜딩페이지 수정

--- src/pages/LandingPage/Carousel1.tsx
+++ src/pages/LandingPage/Carousel1.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-import { useNavigate } from "react-router-dom";
-import * as styled from "./styles";
+import {
+  Container,
+  SubText2,
+  Stack,
+  StyledMainText2,
+  CenterFlexBox,
+  Con2DivforImage,
+  Box,
+  SectionWrapper,
+} from "./styles";
 
 import beer from "../../images/beer.png";
 import car from "../../images/car.png";
@@ -12,46 +20,52 @@ import gift from "../../images/gift.png";
 import house from "../../images/house.png";
 import meet from "../../images/meet.png";
 import money from "../../images/money.png";
+import { HeaderOffset } from "../../components/Header/styles";
 
 const Carousel1: React.FC = () => {
   const images = [beer, car, carrier, cart, clothes, gift, house, meet, money];
-  console.log("Carousel1 rendered");
-  return (
-    <styled.Container2>
-      <div>
-        <styled.Con2MainText>
-          매일매일
-          <br />
-          목표로
-          <br /> 차근차근
-        </styled.Con2MainText>
-        <div>
-          <styled.Con2SubText>
-            목표를 내가 등록할 수 있어요. <br />
-            금액은 자유롭게 선택할 수 있어요.
-          </styled.Con2SubText>
-        </div>
-      </div>
 
-      <styled.Con2DivforBox>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(3, 1fr)", // 3열로 설정
-            gap: "30px", // 박스 간격
-          }}
-        >
-          {images.map((imgSrc, index) => (
-            <styled.Box key={index}>
-              <styled.Con2DivforImage
-                src={imgSrc}
-                alt={`아이템 ${index + 1}`}
-              />
-            </styled.Box>
-          ))}
+  return (
+    <Container>
+      <SectionWrapper style={{ backgroundColor: "#006b6b" }}>
+        <div style={{ width: "100%", height: "100%", display: "flex" }}>
+          <div style={{ flex: 1 }}>
+            <CenterFlexBox>
+              <Stack>
+                <StyledMainText2>
+                  매일매일
+                  <br />
+                  목표로
+                  <br /> 차근차근
+                </StyledMainText2>
+                <SubText2>
+                  목표를 내가 등록할 수 있어요. <br />
+                  금액은 자유롭게 선택할 수 있어요.
+                </SubText2>
+              </Stack>
+            </CenterFlexBox>
+          </div>
+          <div style={{ flex: 1 }}>
+            <CenterFlexBox>
+              <div
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "repeat(3, 1fr)", // 3열로 설정
+                  gap: "30px", // 박스 간격
+                }}
+              >
+                {images.map((imgSrc, index) => (
+                  <Box key={index}>
+                    <Con2DivforImage src={imgSrc} alt={`아이템 ${index + 1}`} />
+                  </Box>
+                ))}
+              </div>
+            </CenterFlexBox>
+          </div>
         </div>
-      </styled.Con2DivforBox>
-    </styled.Container2>
+        <HeaderOffset></HeaderOffset>
+      </SectionWrapper>
+    </Container>
   );
 };
 
