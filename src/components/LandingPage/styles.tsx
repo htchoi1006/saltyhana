712eac1f752e0153d33913956a3580c0ff1b40bc
magae1 <lws6665@naver.com>
2024-10-26 13:48:34 +0900

    fix. 랜딩페이지 수정

--- src/components/LandingPage/styles.tsx
+++ src/components/LandingPage/styles.tsx
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: auto;
-  height: 100vh;
   background-color: #e9edf08f;
 `;
--- src/pages/LandingPage/Carousel2.tsx
+++ src/pages/LandingPage/Carousel2.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import * as styled from "./styles";
+import {
+  Container,
+  SectionWrapper,
+  CenterFlexBox,
+  Stack,
+  ElementImg,
+  ElementsWrapper,
+  ConsumptionTestWrapper,
+  Con22h1,
+  Con22p,
+  Con22p2,
+  Con22Button,
+} from "./styles";
 import img1 from "../../images/TeststartImg1.png";
 import img2 from "../../images/TeststartImg2.png";
 import img3 from "../../images/TeststartImg3.png";
+import { HeaderOffset } from "../../components/Header/styles";
+
 const Carousel2: React.FC = () => {
   const navigate = useNavigate();
 
@@ -13,51 +28,61 @@ const Carousel2: React.FC = () => {
   };
 
   return (
-    <styled.Container22>
-      <div>
-        <div>
-          <styled.Con2MainText>
-            나만의
-            <br />
-            소비성향
-            <br /> 찾아보기
-          </styled.Con2MainText>
+    <Container>
+      <SectionWrapper style={{ backgroundColor: "#0063b2" }}>
+        <div style={{ width: "100%", height: "100%", display: "flex" }}>
+          <div style={{ flex: 1 }}>
+            <CenterFlexBox>
+              <Stack>
+                <div>
+                  <div>
+                    <styled.Con2MainText>
+                      나만의
+                      <br />
+                      소비성향
+                      <br /> 찾아보기
+                    </styled.Con2MainText>
+                  </div>
+                  <div>
+                    <styled.Con22SubText>
+                      소비 성향을 분석하고
+                      <br /> 내가 어떤 타입에 속하는지 확인해보세요.
+                      <br />
+                      나에게는 어떤 금융 상품이 적합할까요?
+                    </styled.Con22SubText>
+                  </div>
+                </div>
+              </Stack>
+            </CenterFlexBox>
+          </div>
+          <div style={{ flex: 1 }}>
+            <CenterFlexBox>
+              <ConsumptionTestWrapper>
+                <Con22h1>내 소비 성향 찾기</Con22h1>
+                <Con22p>
+                  내 소비 성향을 찾고 맞춤형 금융 상품을 찾을 수 있어요
+                </Con22p>
+                <ElementsWrapper>
+                  <Stack>
+                    <ElementImg src={img1} />
+                    <Con22p2>8개 질문으로 간단하게!</Con22p2>
+                  </Stack>
+                  <Stack>
+                    <ElementImg src={img2} />
+                    <Con22p2>소비 성향을 분석!</Con22p2>
+                  </Stack>
+                  <Stack>
+                    <ElementImg src={img3} />
+                    <Con22p2>맞춤형 금융 상품 추천까지!</Con22p2>
+                  </Stack>
+                </ElementsWrapper>
+              </ConsumptionTestWrapper>
+            </CenterFlexBox>
+          </div>
         </div>
-        <div>
-          <styled.Con22SubText>
-            소비 성향을 분석하고
-            <br /> 내가 어떤 타입에 속하는지 확인해보세요.
-            <br />
-            나에게는 어떤 금융 상품이 적합할까요?
-          </styled.Con22SubText>
-        </div>
-      </div>
-      <styled.Con22Box>
-        <styled.Con22h1>내 소비 성향 찾기</styled.Con22h1>
-        <styled.Con22p>
-          내 소비 성향을 찾고 맞춤형 금융 상품을 찾을 수 있어요
-        </styled.Con22p>
-
-        <styled.Element1>
-          <styled.ElementImg src={img1} />
-          <p>8개 질문으로 간단하게!</p>
-        </styled.Element1>
-
-        <styled.Element2>
-          <styled.ElementImg src={img2} />
-          <styled.Con22p2>소비 성향을 분석!</styled.Con22p2>
-        </styled.Element2>
-
-        <styled.Element3>
-          <styled.ElementImg src={img3} />
-          <p>맞춤형 금융 상품 추천까지!</p>
-        </styled.Element3>
-
-        <styled.Con22Button onClick={handleTestStartClick}>
-          시작하기
-        </styled.Con22Button>
-      </styled.Con22Box>
-    </styled.Container22>
+        <HeaderOffset></HeaderOffset>
+      </SectionWrapper>
+    </Container>
   );
 };
 
