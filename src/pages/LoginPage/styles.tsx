2747db01974c35eb44c99b6b11d176628810617d
htchoi1006 <htchoi1006@kookmin.ac.kr>
2024-11-01 23:44:05 +0900

    add. 로그인, 회원가입 페이지 애니메이션 추가

--- src/pages/LoginPage/styles.tsx
+++ src/pages/LoginPage/styles.tsx
@@ -1,7 +1,21 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 export const StyledLink = styled(Link)`
+  opacity: 0;
+  animation: ${fadeIn} 1s ease-out forwards;
+  animation-delay: 1.1s;
   font-style: normal;
   font-weight: 700;
   line-height: 140%;
