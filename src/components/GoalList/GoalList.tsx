93f95de473aa5649ded2aa83f9e9ca71d80e8fd5
commaengddongyeon <kimdongyn927@naver.com>
2024-11-06 23:12:33 +0900

    chore. 캘린더 목표 리스트 애니메이션 수정

--- src/components/GoalList/GoalList.tsx
+++ src/components/GoalList/GoalList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   GoalListContainer,
   GoalItem,
@@ -25,44 +25,51 @@ interface GoalListProps {
 
 export default function GoalList(props: GoalListProps) {
   const { goals, onGoalClick } = props;
+  const [activeGoalId, setActiveGoalId] = useState<string | null>(
+    goals.length > 0 ? String(goals[0].id) : null,
+  ); // 첫 번째 요소를 기본 선택
+
+  const handleGoalClick = (goal: Goal) => {
+    setActiveGoalId(String(goal.id));
+    onGoalClick(goal);
+  };
 
   return (
     <GoalListContainer>
       <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
         {goals.map((goal) => (
-          <>
-            <GoalItem
-              key={goal.id}
-              color={goal.color}
-              onClick={() => onGoalClick(goal)}
-            >
-              <GoalDiv>
-                <GoalInfo>
-                  <GoalTitle>
-                    {goal.title}
-                    <Link to="/goal">
-                      <EditButton src={editIcon} />
-                    </Link>
-                  </GoalTitle>
-                  <GoalDate>
-                    {goal.startDate} - {goal.endDate}
-                  </GoalDate>
-                </GoalInfo>
-                <GoalIcon src={goal.icon} alt="Goal Icon" />
-              </GoalDiv>
-              <ProgressBar>
-                <ProgressFill
-                  style={{
-                    width: `${goal.progress}%`,
-                    bottom: "50px",
-                    backgroundColor: goal.color,
-                    opacity: "0.5",
-                  }}
-                />
-              </ProgressBar>
-              <DeleteButton src={deleteIcon} />
-            </GoalItem>
-          </>
+          <GoalItem
+            key={goal.id}
+            color={goal.color}
+            isActive={String(goal.id) === activeGoalId}
+            onClick={() => handleGoalClick(goal)}
+          >
+            <GoalDiv>
+              <GoalInfo>
+                <GoalTitle>
+                  {goal.title}
+                  <Link to="/goal">
+                    <EditButton src={editIcon} />
+                  </Link>
+                </GoalTitle>
+                <GoalDate>
+                  {goal.startDate} - {goal.endDate}
+                </GoalDate>
+              </GoalInfo>
+              <GoalIcon src={goal.icon} alt="Goal Icon" />
+            </GoalDiv>
+            <ProgressBar>
+              <ProgressFill
+                style={{
+                  width: `${goal.progress}%`,
+                  bottom: "50px",
+                  backgroundColor: goal.color,
+                  opacity: "0.5",
+                }}
+              />
+            </ProgressBar>
+            <DeleteButton src={deleteIcon} />
+          </GoalItem>
         ))}
       </div>
       <div>
