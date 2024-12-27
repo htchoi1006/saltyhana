7cbc04ce166ae4caadafc3f2bc2f991f0792aac3
htchoi1006 <htchoi1006@kookmin.ac.kr>
2024-12-27 12:51:38 +0900

    add. 상품 추천 목록에 상품 추천 이유 띄워주는 기능 구현

--- src/pages/RecommendPage/styles.tsx
+++ src/pages/RecommendPage/styles.tsx
@@ -46,8 +46,89 @@ export const PageDescription = styled.p`
   font-weight: 400;
   font-size: 16px;
   padding-bottom: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
+export const InformationIcon = styled.img`
+  width: 20px;
+  height: auto;
+  cursor: pointer;
+  margin-right: 30px;
+`;
+
+// styles.ts에 추가
+export const InfoIconWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  margin-left: 10px;
+`;
+
+// export const InfoTooltip = styled.div`
+// 	font-family: "Noto Sans KR";
+// 	position: absolute;
+// 	z-index: 50;
+// 	width: 300px;
+// 	padding: 12px;
+// 	background-color: rgba(52, 52, 52, 0.8);
+// 	border-radius: 8px;
+// 	top: calc(50% + 20px);
+// 	left: calc(100%); // InformationIcon 오른쪽으로 10px 띄움
+// 	transform: translateY(-50%); // 세로 중앙 정렬
+
+// 	&:after {
+// 		content: "";
+// 		position: absolute;
+// 		left: -6px; // 왼쪽으로 화살표 이동
+// 		top: 40%;
+// 		width: 12px;
+// 		height: 12px;
+// 		background-color: rgba(52, 52, 52, 0.8);
+// 		transform: translateY(-50%) rotate(45deg); // 화살표 왼쪽 방향으로 변경
+// 	}
+
+// 	p {
+// 		margin: 0;
+// 		font-size: 13px;
+// 		line-height: 1.5;
+// 		color: white;
+// 		word-break: keep-all;
+// 	}
+// `;
+
+export const InfoTooltip = styled.div`
+  position: absolute;
+  z-index: 50;
+  width: 300px;
+  padding: 12px;
+  background-color: rgba(52, 52, 52, 0.75);
+  border-radius: 8px;
+  top: calc(50% + 20px);
+  left: calc(95% - 15px); // 화살표 공간 확보를 위해 약간 조정
+  transform: translateY(-50%);
+
+  &:after {
+    content: "";
+    position: absolute;
+    left: -8px; // 크기에 맞게 조정
+    top: 38%;
+    transform: translateY(-50%);
+    border-top: 8px solid transparent;
+    border-bottom: 8px solid transparent;
+    border-right: 8px solid rgba(52, 52, 52, 0.75);
+    border-left: none;
+  }
+
+  p {
+    margin: 0;
+    font-size: 13px;
+    line-height: 1.5;
+    color: white;
+    word-break: keep-all;
+    white-space: pre-line;
+  }
+`;
 export const TestCardWrapper = styled.div`
   display: flex;
   justify-content: center;
